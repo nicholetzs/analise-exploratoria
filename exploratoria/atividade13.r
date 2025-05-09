65f5b5d2e091f36bbded1fed0244e346d92d5738
Nichole <nchnicolini@gmail.com>
2025-05-09 17:50:14 -0300

    incluindo todas as analises exploratorias

--- exploratoria/atividade13.r
+++
@@ -1,8 +0,0 @@
-# (a)
-prob_13a <- pnorm(170000, mean = 150000, sd = 5000)
-cat(sprintf("Exercício 13(a): P(X < 170000) = %.4f\n", prob_13a))
-
-
-# (b)
-prob_13b <- pnorm(165000, mean = 150000, sd = 5000) - pnorm(140000, mean = 150000, sd = 5000)
-cat(sprintf("Exercício 13(b): P(140000 ≤ X ≤ 165000) = %.4f\n", prob_13b))
